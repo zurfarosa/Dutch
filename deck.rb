38b6b5a5bfecbf65becf76e88654ea168251a6ad
zurfarosa <richard.hoile@gmail.com>
2018-09-01 23:13:34 +0100

    now printing to correct size

--- deck.rb
+++ deck.rb
@@ -1,23 +1,26 @@
 require 'squib'
+require 'game_icons'
 
 data = Squib.csv file:'data.csv'
-puts data['title']
 
-Squib::Deck.new cards: data['bid_value'].size, layout: 'layout.yml' do
-  background color: 'white'
+Squib::Deck.new cards: data['bid'].size, layout: 'layout.yml', width:'62.4mm', height:'92.4mm' do
   rect layout: 'suit_band'
-  cut_zone margin:36
-  safe_zone margin: 72
-  # svg file: data['title'].map { |t| "icons/#{t.downcase}.svg"}, layout:'main_image'
-  # text str: data['title'], layout:'title'
-  text layout:'bid', str: data['bid_value'].map{|i| "#{i} :coins:"} do |embed|
-    embed.png key: ':coins:', file: 'icons/roman_coin_2.png', height:140, width:140
+  rect layout: 'bleed', radius: '2mm'
+  safe_zone layout:'safe'
+  text layout:'bid', str: data['bid'].map{|i| ":bid:  #{i}"} do |embed|
+      embed.svg key: ':bid:', file: GameIcons.get('thor-hammer').file, layout:'description_icon'
   end
-  text layout:'vp', str: data['vp_value'].map{|i| "#{i.abs} :laurel:"} do |embed|
-    embed.svg key: ':laurel:', file: 'icons/laurel.svg', height:140, width:140
+
+  text layout:'vp', str: data['vp'].map{|i| ":vp:  #{i}"} do |embed|
+      embed.svg key: ':vp:', file: GameIcons.get('laurel-crown').file, layout:'description_icon'
   end
-  text layout:'vp negative sign', str: data['vp_value'].map{|i| "-" if i<0}
-  text str: data['special_power'].map{|i| "or <span >#{i}</span>" if i != nil}, layout:'special_power'
+
+  text layout:'power', str: data['power'].map{|i| ":power:  #{i}" if i != nil} do |embed|
+      embed.svg key: ':power:', file: GameIcons.get('swiss-army-knife').file, layout:'description_icon'
+  end
+
+  # text layout:'vp negative sign', str: data['vp_value'].map{|i| "-" if i<0}
+  # text str: data['special_power'].map{|i| " <span >#{i}</span>" if i != nil}, layout:'special_power'
   save_png
-  save_pdf trim: 36, width: '290mm', height: '210mm'
+  save_pdf width: '210mm', height: '297mm', trim: '3.2mm'
 end

baa6650449aedc95015e548220689e3c8f4ad1ca
zurfarosa <richard.hoile@gmail.com>
2018-09-04 22:26:25 +0100

    classical style

--- deck.rb
+++ deck.rb
@@ -5,19 +5,19 @@ data = Squib.csv file:'data.csv'
 
 Squib::Deck.new cards: data['bid'].size, layout: 'layout.yml', width:'62.4mm', height:'92.4mm' do
   rect layout: 'suit_band'
+  png layout: 'bid_circle'
+  # rect layout: 'power_box'
   rect layout: 'bleed', radius: '2mm'
-  safe_zone layout:'safe'
-  text layout:'bid', str: data['bid'].map{|i| ":bid:  #{i}"} do |embed|
-      embed.svg key: ':bid:', file: GameIcons.get('thor-hammer').file, layout:'description_icon'
-  end
+  # safe_zone layout:'safe'
+  text layout:'bid', str: data['bid'].map{|i| "#{i}"}
+  # svg file: GameIcons.get('thor-hammer').file, layout:'bid_icon'
 
-  text layout:'vp', str: data['vp'].map{|i| ":vp:  #{i}"} do |embed|
-      embed.svg key: ':vp:', file: GameIcons.get('laurel-crown').file, layout:'description_icon'
-  end
+  svg layout:'vp_frame'
+  text layout:'vp', str: data['vp'].map{|i| "#{i}"}
+  # svg file: GameIcons.get('laurel-crown').file, layout:'vp_icon'
 
-  text layout:'power', str: data['power'].map{|i| ":power:  #{i}" if i != nil} do |embed|
-      embed.svg key: ':power:', file: GameIcons.get('swiss-army-knife').file, layout:'description_icon'
-  end
+  text layout:'power', str: data['power'].map{|i| "#{i}" if i != nil}
+  # svg file: GameIcons.get('swiss-army-knife').file, layout:'power_icon'
 
   # text layout:'vp negative sign', str: data['vp_value'].map{|i| "-" if i<0}
   # text str: data['special_power'].map{|i| " <span >#{i}</span>" if i != nil}, layout:'special_power'

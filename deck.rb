36499eb013cc4c3909b01e0613e0823321dbe480
zurfarosa <richard.hoile@gmail.com>
2018-09-06 00:44:42 +0100

    added different colours

--- deck.rb
+++ deck.rb
@@ -3,24 +3,20 @@ require 'game_icons'
 
 data = Squib.csv file:'data.csv'
 
-Squib::Deck.new cards: data['bid'].size, layout: 'layout.yml', width:'62.4mm', height:'92.4mm' do
-  rect layout: 'suit_band'
-  png layout: 'bid_circle'
-  # rect layout: 'power_box'
-  rect layout: 'bleed', radius: '2mm'
-  # safe_zone layout:'safe'
-  text layout:'bid', str: data['bid'].map{|i| "#{i}"}
-  # svg file: GameIcons.get('thor-hammer').file, layout:'bid_icon'
+for colour in ['light pink','light blue', '#228B22', '#CD2626','burnt orange']
+  Squib::Deck.new cards: data['bid'].size, layout: 'layout.yml', width:'62.4mm', height:'92.4mm' do
+    background color: :white
+    rect layout: 'suit_band', fill_color: colour
+    png layout: 'bid_circle'
+    # rect layout: 'power_box'
+    rect layout: 'bleed'
+    # safe_zone layout:'safe'
+    text layout:'bid', str: data['bid'].map{|i| "#{i}"}
 
-  svg layout:'vp_frame'
-  text layout:'vp', str: data['vp'].map{|i| "#{i}"}
-  # svg file: GameIcons.get('laurel-crown').file, layout:'vp_icon'
-
-  text layout:'power', str: data['power'].map{|i| "#{i}" if i != nil}
-  # svg file: GameIcons.get('swiss-army-knife').file, layout:'power_icon'
-
-  # text layout:'vp negative sign', str: data['vp_value'].map{|i| "-" if i<0}
-  # text str: data['special_power'].map{|i| " <span >#{i}</span>" if i != nil}, layout:'special_power'
-  save_png
-  save_pdf width: '210mm', height: '297mm', trim: '3.2mm'
+    svg layout:'vp_frame'
+    text layout:'vp', str: data['vp'].map{|i| "#{i}"}
+    text layout:'power', str: data['power'].map{|i| "#{i}" if i != nil}
+    save_png prefix:"card_ #{colour}_", trim:'3.2mm', trim_radius:'2mm'
+    save_pdf file: "card_ #{colour}.pdf", width: '210mm', height: '297mm', trim: '3.2mm'
+  end
 end

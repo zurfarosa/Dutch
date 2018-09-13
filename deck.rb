f00c6c086d1abebb42cd3070158dd931196f26e5
zurfarosa <richard.hoile@gmail.com>
2018-09-13 14:56:44 +0100

    mini cards created

--- deck.rb
+++ deck.rb
@@ -21,18 +21,33 @@ end
 
 data = Squib.csv file:'data_expanded.csv'
 
-
+#create the main deck of cards
 Squib::Deck.new cards: data['bid'].size, layout: 'layout.yml', width:'62.4mm', height:'92.4mm' do
   background color: :white
   rect layout: 'suit_band', fill_color: data['colour']
   png layout: 'bid_circle'
   rect layout: 'bleed'
+  # rect layout: 'inner_border'
   # safe_zone layout:'safe'
-  text layout:'bid', str: data['bid'].map{|i| "#{i}"}
+  text layout:'bid_value', str: data['bid'].map{|i| "#{i}"}
 
-  png layout:'vp_frame'
-  text layout:'vp', str: data['vp'].map{|i| "#{i}"}
-  text layout:'power', str: data['power'].map{|i| "#{i}" if i != nil}
-  save_png prefix:"card_", trim:'3.2mm', trim_radius:'2mm'
-  save_pdf file: "cards.pdf", width: '210mm', height: '297mm', trim: '3.2mm'
+  svg layout:'vp_frame'
+  text layout:'vp_value', str: data['vp'].map{|i| "#{i}"}
+  text layout:'power_text', str: data['power'].map{|i| "#{i}" if i != nil}
+  save_png prefix:"card_", layout:'png_dims'
+  save_pdf file: "cards.pdf", width: '210mm', height: '297mm', trim: '3.2mm'#layout:'pdf_dims'
+end
+
+#now create the mini-cards without power text for creating diagrams for the instructions
+Squib::Deck.new cards: data['bid'].size, layout: 'layout.yml', width:'31.2mm', height:'46.2mm'  do
+  background color: :white
+  rect layout: 'mini_suit_band', fill_color: data['colour']
+  png layout: 'mini_bid_circle'
+  text layout:'mini_bid_value', str: data['bid'].map{|i| "#{i}"}
+
+  svg layout:'mini_vp_frame'
+  text layout:'mini_vp_value', str: data['vp'].map{|i| "#{i}"}
+  text layout:'mini_power_text', str: data['power'].map{|i| "#{i}" if i != nil}
+
+  save_png prefix:"mini_card_", layout:'mini_png_dims'
 end
